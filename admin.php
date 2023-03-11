9451f855f02f951d92446aaf9b543c09b2116a5d
Hassan-Mehmood <hassanafridi1913@gmail.com>
2023-03-11 20:19:24 +0500

    i dont know what i am doing

--- admin.php
+++ admin.php
@@ -1,18 +1,18 @@
 <?php require 'header.php';
 
-
 // Checking if the admin is logged in
-if (isset($_SESSION['user'])) {
-  print "session exists";
-} else {
-  // If adming is not loggedin redirect to the login page
-  header("Location: http://localhost/Restraunt%20management%20system/login.php");
-}
-
+// if (isset($_SESSION['user'])) {
+//   print "session exists";
+// } else {
+//   // If adming is not loggedin redirect to the login page
+//   header("Location: http://localhost/Restaurant%20management%20system/login.php");
+// }
 
 ?>
 
-<h1 style="margin-top: 10rem;">Hello WOrld</h1>
-
+<section class="add_item_form max_width">
+  <form class="modal-content" action="" method="post">
+  </form>
+</section>
 
 <?php require 'footer.php' ?>
--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php require 'header.php'; ?>
 
-<section class="login_form max_width">
+<section class="form max_width">
   <div>
     <form class="modal-content" action="" method="post">
       <div class="container">
         <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+        <input type="text" placeholder="Enter Username" name="uname" class="username" required>
 
         <label for="password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
+        <input type="password" placeholder="Enter Password" name="password" class="password" required>
 
         <button type="submit" class='login_form_button'>Login</button>
       </div>
