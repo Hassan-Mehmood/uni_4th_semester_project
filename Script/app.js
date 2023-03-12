28247122be12a39cc19c0ce133aa9ea99946b293
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-03-12 13:52:38 +0500

    add menu section done

--- Script/app.js
+++ Script/app.js
@@ -1,11 +1,29 @@
 const nav = document.getElementsByClassName('nav')[0];
+const addMenuItemBtn = document.getElementById('add_item_btn');
+const addMenuItemSection = document.getElementsByClassName('add_menu_item')[0];
+const close_icon_img = document.getElementsByClassName('close_icon_img')[0];
+
+console.log(addMenuItemBtn);
+console.log(addMenuItemSection);
 
 window.addEventListener('scroll', handleScrollEvent);
+addMenuItemBtn.addEventListener('click', showAddMenuItemForm);
+close_icon_img.addEventListener('click', closeAddMenuItemForm);
 
 function handleScrollEvent() {
   if (window.scrollY > 60) {
-    nav.className = 'bg-black';
+    nav.classList.add('bg-black');
   } else {
-    nav.className = '';
+    nav.classList.remove('bg-black');
   }
+}
+
+function showAddMenuItemForm(e) {
+  e.preventDefault();
+  addMenuItemSection.classList.remove('hidden');
+}
+
+function closeAddMenuItemForm(e) {
+  e.preventDefault();
+  addMenuItemSection.classList.add('hidden');
 }
--- admin.php
+++ admin.php
@@ -10,37 +10,60 @@
 
 ?>
 
-<section class="admin_menu_section max_width" id="menu">
-  <div class="add_menu_item_button">
-    <button>Add Item</button>
-  </div>
-  <div class="admin_menu_items">
-    <?php for ($i = 0; $i < 5; $i++) { ?>
-      <div class="admin_menu_item">
-        <div class="item_info">
-          <div class="menu_item_img">
-            <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
+<div class="max_width">
+
+  <section class="admin_menu_section " id="menu">
+    <div class="add_menu_item_button">
+      <button id="add_item_btn">Add Item</button>
+    </div>
+    <div class="admin_menu_items">
+      <?php for ($i = 0; $i < 5; $i++) { ?>
+        <div class="admin_menu_item">
+          <div class="item_info">
+            <div class="menu_item_img">
+              <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
+            </div>
+            <div class="menu_item_text">
+              <h4 class="menu_item_name">Salmon Roll</h4>
+              <span class="menu_item_price">$18</span>
+              <p class="menu_item_detail">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
+              </p>
+            </div>
           </div>
-          <div class="menu_item_text">
-            <h4 class="menu_item_name">Salmon Roll</h4>
-            <span class="menu_item_price">$18</span>
-            <p class="menu_item_detail">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-            </p>
+          <div>
+            <button>Edit</button>
+            <button>Delete</button>
           </div>
         </div>
-        <div>
-          <button>Edit</button>
-          <button>Delete</button>
+      <?php } ?>
+    </div>
+  </section>
+
+  <section class="add_menu_item hidden">
+    <form class="modal-content" action="" method="post">
+      <div class="container">
+        <div class="close_icon">
+          <img src="./Images/close.png" alt="Close" class="close_icon_img">
         </div>
+        <label for="item_name"><b>Item Name</b></label>
+        <input type="text" name="item_name" class="item_name" id="item_name" required>
+
+        <label for="item_price"><b>Price</b></label>
+        <input type="price" name="price" class="item_price" id="item_price" required>
+
+        <label for="item_description"><b>Description</b></label>
+        <textarea type="Description" name="description" class="item_Description" id="item_description" required> </textarea>
+
+        <label for="item_category"><b>Category</b></label>
+        <input type="category" name="category" class="item_category" id="item_category" required>
+
+        <button type="submit" class='login_form_button'>
+          Add Item
+        </button>
       </div>
-    <?php } ?>
-  </div>
-</section>
-
-<section class="add_menu_item">
-
-</section>
-
+    </form>
+  </section>
+</div>
 
 <?php require 'footer.php' ?>
