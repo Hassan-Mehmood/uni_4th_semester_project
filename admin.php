bc3d74bfd0d4a33d7955bb8c0d0e2ea0f063fd37
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-03-12 12:24:38 +0500

    admin menu done

--- admin.php
+++ admin.php
@@ -10,9 +10,37 @@
 
 ?>
 
-<section class="add_item_form max_width">
-  <form class="modal-content" action="" method="post">
-  </form>
+<section class="admin_menu_section max_width" id="menu">
+  <div class="add_menu_item_button">
+    <button>Add Item</button>
+  </div>
+  <div class="admin_menu_items">
+    <?php for ($i = 0; $i < 5; $i++) { ?>
+      <div class="admin_menu_item">
+        <div class="item_info">
+          <div class="menu_item_img">
+            <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
+          </div>
+          <div class="menu_item_text">
+            <h4 class="menu_item_name">Salmon Roll</h4>
+            <span class="menu_item_price">$18</span>
+            <p class="menu_item_detail">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
+            </p>
+          </div>
+        </div>
+        <div>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
+    <?php } ?>
+  </div>
 </section>
+
+<section class="add_menu_item">
+
+</section>
+
 
 <?php require 'footer.php' ?>
