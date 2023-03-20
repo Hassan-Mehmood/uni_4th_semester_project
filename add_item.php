074c8beee5876a247f73d24204ca3b9435591481
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-03-20 22:43:08 +0500

    working on file upload

--- add_item.php
+++ add_item.php
@@ -14,30 +14,38 @@ if (isset($_POST['add_item'])) {
   $price = htmlspecialchars($_POST['price']);
   $description = htmlspecialchars($_POST['description']);
   $category = htmlspecialchars($_POST['category']);
+  $image = $_FILES['mainImage'];
 
-  if (empty(trim($itemName)) || empty(trim($price)) || empty(trim($description)) || empty(trim($category))) {
-    $form_error = 'Empty Fields';
-  } else if (!preg_match('/^[0-9]*$/', $price)) {
-    $form_error = 'Price should be a number';
-  } else {
-    $sql_query = "INSERT INTO item (name, description, price, category) VALUES (?, ?, ?, ?)";
+  $target_dir = "uploads/";
+  $target_file = $target_dir . basename($_FILES["mainImage"]["name"]);
+  $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    if ($stmt = mysqli_prepare($conn, $sql_query)) {
-      mysqli_stmt_bind_param($stmt, "ssis", $itemName, $description, $price, $category);
-      mysqli_stmt_execute($stmt);
-      $result = mysqli_stmt_get_result($stmt);
+  $check = getimagesize($_FILES["mainImage"]["name"]);
+  if (!$check) $form_error = "File is not an image.";
 
-      $form_response = 'Item added';
-      header("Location: http://localhost/Restraunt%20management%20system/admin.php");
-    } else {
-      $form_response = 'Something went wrong';
-    }
-  }
+  // if (empty(trim($itemName)) || empty(trim($price)) || empty(trim($description)) || empty(trim($category))) {
+  //   $form_error = 'Empty Fields';
+  // } else if (!preg_match('/^[0-9]*$/', $price)) {
+  //   $form_error = 'Price should be a number';
+  // } else {
+  //   $sql_query = "INSERT INTO item (name, description, price, category) VALUES (?, ?, ?, ?)";
+
+  //   if ($stmt = mysqli_prepare($conn, $sql_query)) {
+  //     mysqli_stmt_bind_param($stmt, "ssis", $itemName, $description, $price, $category);
+  //     mysqli_stmt_execute($stmt);
+  //     $result = mysqli_stmt_get_result($stmt);
+
+  //     $form_response = 'Item added';
+  //     header("Location: http://localhost/Restraunt%20management%20system/admin.php");
+  //   } else {
+  //     $form_response = 'Something went wrong';
+  //   }
+  // }
 }
 ?>
 
 <section class="add_menu_item">
-  <form class="modal-content" action="" method="post">
+  <form class="modal-content" action="" method="post" enctype="multipart/form-data">
     <div class="container">
       <a href="admin.php">Back</a>
       <div class="menu_header">
@@ -54,6 +62,19 @@ if (isset($_POST['add_item'])) {
 
       <label for="item_category"><b>Category</b></label>
       <input type="category" name="category" class="item_category" id="item_category">
+
+      <div class="imageContainer">
+        <label class="imageLabel" id="mainImageLabel">
+          <!-- <img src="./Images/image_icon.png" alt=""> -->
+          Upload the SVG, PNG, JPG etc
+        </label>
+        <img id="mainUploadPreview" />
+        <label for="mainImage" class="imageUpload">
+          Browse
+        </label>
+        <input type="file" id="mainImage" name="mainImage" />
+      </div>
+
       <span class="admin_login_error"><?= $form_error; ?></span>
       <span class="add_item_form_response"><?= $form_response; ?></span>
       <button type="submit" class='login_form_button' name="add_item">
--- menu.php
+++ menu.php
@@ -1,153 +1,35 @@
-<?php require 'header.php' ?>
+<?php
+require 'header.php';
+
+$sql_query = 'SELECT * FROM item';
+$result = mysqli_query($conn, $sql_query);
+?>
 
 <section class="menu_section max_width" id="menu">
-  <h3 class="menu_welcome">Welcome</h3>
   <h2 class="menu_heading">Our Menu</h2>
   <div class="menu_items">
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Salmon Roll</h4>
-        <span class="menu_item_price">$18</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item2.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Sushi</h4>
-        <span class="menu_item_price">$18</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Salmon Roll</h4>
-        <span class="menu_item_price">$25</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item2.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Sushi</h4>
-        <span class="menu_item_price">$25</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Salmon Roll</h4>
-        <span class="menu_item_price">$18</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item2.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Sushi</h4>
-        <span class="menu_item_price">$18</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Salmon Roll</h4>
-        <span class="menu_item_price">$25</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item2.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Sushi</h4>
-        <span class="menu_item_price">$25</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Salmon Roll</h4>
-        <span class="menu_item_price">$18</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item2.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Sushi</h4>
-        <span class="menu_item_price">$18</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Salmon Roll</h4>
-        <span class="menu_item_price">$25</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
-    <div class="menu_item">
-      <div class="menu_item_img">
-        <img src="./Images/menu_item2.jpg" alt="Food Item Image" />
-      </div>
-      <div class="menu_item_text">
-        <h4 class="menu_item_name">Sushi</h4>
-        <span class="menu_item_price">$25</span>
-        <p class="menu_item_detail">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, nisi.
-        </p>
-      </div>
-    </div>
+    <?php
+    if (mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_assoc($result)) { ?>
+        <div class="menu_item">
+          <div class="menu_item_img">
+            <img src="./Images/menu_item1.jpg" alt="Food Item Image" />
+          </div>
+          <div class="menu_item_text">
+            <div>
+              <h4 class="menu_item_category"><?= $row['category'] ?></h4>
+            </div>
+            <div>
+              <h4 class="menu_item_name"><?= $row['name'] ?></h4>
+              <span class="menu_item_price">$<?= $row['price'] ?></span>
+              <p class="menu_item_detail">
+                <?= $row['description'] ?>
+              </p>
+            </div>
+          </div>
+        </div>
+    <?php }
+    } ?>
   </div>
 </section>
 
